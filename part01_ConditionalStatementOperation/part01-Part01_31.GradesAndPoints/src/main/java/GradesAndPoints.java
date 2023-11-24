75382f8cf68ac3291f29575feb893725f754515f
yiersan <133016840+yier3an@users.noreply.github.com>
2023-11-25 01:27:13 +0800

    upload part 1.24 to 1.36

---
+++ part01_ConditionalStatementOperation/part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String grade;
+
+        System.out.println("Give points [0-100]:");
+        int points = Integer.valueOf(scan.nextLine());
+
+        if (points < 0) {
+            grade = "impossible!";
+        } else if (points < 50) {
+            grade = "failed";
+        } else if (points < 60) {
+            grade = "1";
+        } else if (points < 70) {
+            grade = "2";
+        } else if (points < 80) {
+            grade = "3";
+        } else if (points < 90) {
+            grade = "4";
+        } else if (points <= 100) {
+            grade = "5";
+        } else {
+            grade = "incredible!";
+        }
+
+        System.out.println("Grade: " + grade);
+    }
+}
---
+++ part01_ConditionalStatementOperation/part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give a number:");
+        int no = Integer.valueOf(scan.nextLine());
+        boolean isEven = (no % 2) == 0;
+        String result;
+
+        if (isEven) {
+            result = "even";
+        } else {
+            result = "odd";
+        }
+
+        System.out.println("Number " + no + " is " + result + ".");
+    }
+}
